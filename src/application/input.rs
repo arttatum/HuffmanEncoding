053a67af77eb4754e60b7eda6b40a18bf72c216f
Will Loughney <woughney1@gmail.com>
2023-07-21 22:12:37 +0000

    Add unit tests, decouple parsing from input via BufRead generic constraint.

--- src/application/input.rs
+++ src/application/input.rs
@@ -1,17 +1,29 @@
 use std::collections::HashMap;
 use std::io::BufRead;
-pub struct TextProvider<TokenType> {
-    text: String,
-    frequencies: HashMap<TokenType, u32>,
+
+pub struct Input<R> {
+    reader: R,
 }
 
-impl TextProvider<char> {
-    pub fn from_stdin() -> Self {
+pub struct Summary<T> {
+    pub text: String,
+    pub frequencies: HashMap<T, u32>,
+}
+
+impl<R> Input<R>
+where
+    R: BufRead,
+{
+    pub fn from_source(source: R) -> Self {
+        Input { reader: source }
+    }
+
+    pub fn process_as_chars(&mut self) -> Summary<char> {
         println!("Reading text from stdin into memory and computing character frequency.");
         let mut frequencies = HashMap::new();
         let mut line = String::new();
         let mut text = String::new();
-        while let Ok(n_bytes) = std::io::stdin().lock().read_line(&mut line) {
+        while let Ok(n_bytes) = self.reader.read_line(&mut line) {
             if n_bytes == 0 {
                 break;
             }
@@ -25,14 +37,69 @@ impl TextProvider<char> {
             line.clear();
         }
 
-        TextProvider { text, frequencies }
+        Summary { text, frequencies }
     }
 
-    pub fn get_text(&self) -> &str {
-        &self.text
+    pub fn process_as_strings(&mut self) -> Summary<String> {
+        println!("Reading text from stdin into memory and computing character frequency.");
+        let mut frequencies = HashMap::new();
+        let mut line = String::new();
+        let mut text = String::new();
+        while let Ok(n_bytes) = self.reader.read_line(&mut line) {
+            if n_bytes == 0 {
+                break;
+            }
+            for word in line.split_inclusive(" ").into_iter() {
+                frequencies
+                    .entry(String::from(word))
+                    .and_modify(|value| *value += 1)
+                    .or_insert(1);
+                text.push_str(word);
+            }
+            line.clear();
+        }
+
+        Summary {
+            text: String::from(text),
+            frequencies,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process_as_chars() {
+        let input_text = b"Hello world!";
+        let mut input = Input::from_source(&input_text[..]);
+        let char_summary = input.process_as_chars();
+        assert_eq!(
+            char_summary.text,
+            String::from_utf8(input_text.to_vec()).unwrap()
+        );
+        assert_eq!(char_summary.frequencies[&'H'], 1);
+        assert_eq!(char_summary.frequencies[&'e'], 1);
+        assert_eq!(char_summary.frequencies[&'l'], 3);
+        assert_eq!(char_summary.frequencies[&'o'], 2);
+        assert_eq!(char_summary.frequencies[&'w'], 1);
+        assert_eq!(char_summary.frequencies[&'r'], 1);
+        assert_eq!(char_summary.frequencies[&'d'], 1);
+        assert_eq!(char_summary.frequencies[&'!'], 1);
     }
 
-    pub fn get_frequencies(&self) -> &HashMap<char, u32> {
-        &self.frequencies
+    #[test]
+    fn test_process_as_strings() {
+        let input_text = b"Hello world! Hello ";
+        let mut input = Input::from_source(&input_text[..]);
+        let char_summary = input.process_as_strings();
+        assert_eq!(
+            char_summary.text,
+            String::from_utf8(input_text.to_vec()).unwrap()
+        );
+
+        assert_eq!(char_summary.frequencies["Hello "], 2);
+        assert_eq!(char_summary.frequencies["world! "], 1);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 use std::fs;
+
 mod application;
 mod encoding;
 
-use application::input::TextProvider;
-use encoding::huffman::{Tree, Encoder};
+use application::input::Input;
+use encoding::huffman::{Encoder, Tree};
 
 fn main() {
-    let provider = TextProvider::from_stdin();
-    
-    let huffman_tree = Tree::from_frequencies(provider.get_frequencies());
+    let mut input = Input::from_source(std::io::stdin().lock());
+
+    let summary = input.process_as_chars();
+
+    let huffman_tree = Tree::from_frequencies(&summary.frequencies);
 
     let encoder = Encoder::from_huffman_tree(huffman_tree);
 
-    let encoded_text = encoder.encode(provider.get_text());
+    let encoded_text = encoder.encode(&summary.text);
 
     let decoded_text = encoder.decode(&encoded_text);
-
 
     let encoded_text_size = encoded_text.len() / 8 + {
         if encoded_text.len() % 8 == 0 {
@@ -24,7 +26,7 @@ fn main() {
             1
         }
     };
-    println!("Original text consumes {} bytes", provider.get_text().len());
+    println!("Original text consumes {} bytes", &summary.text.len());
     println!("Encoded text consumes {} bytes", encoded_text_size);
 
     println!("Decoded text consumes {} bytes", decoded_text.len());
@@ -34,5 +36,5 @@ fn main() {
         encoded_text_size * 100 / decoded_text.len()
     );
 
-    fs::write("/tmp/huffmanEncoded.txt", decoded_text).unwrap();
+    fs::write("/tmp/huffman_decoded.txt", decoded_text).unwrap();
 }
